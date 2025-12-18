7197e70e907375e339e9479a5aeebf5da4287026
Harrisonust <henhen.luo@gmail.com>
2025-12-18 17:55:27 +0800

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+enum State {
+    Match,
+    Exist,
+    NotExist,
+}
+struct Word {
+    letters: [char; 5],
+    states: [State; 5],
+}
+
+struct Game {
+    round: u8, // maximum 6 rounds
+    answer: String,
+    history: Vec<Word>,
+}
+
+impl Game {
+    fn new() -> Self {
+        Game {
+            round: 0,
+            answer: todo!(), // draw an answer
+            history: Vec::new(),
+        }
+    }
+
+    fn compare(self, user: String) -> Word {
+        Word {
+            letters: todo!(),
+            states: todo!(),
+        }
+    }
+
+    fn update_screen(self) {}
+
+    fn parse_input(self, input: String) {}
+
+    fn run(&mut self) {
+        while self.round < 6 {
+            // take user input
+            let user_input: String = todo!();
+
+            // parsing
+            self.parse_input(user_input);
+
+            // compare
+            let result: Word = self.compare(user_input);
+
+            // save result in history
+            self.history.push(result);
+
+            // update screen
+            self.update_screen();
+
+            self.round += 1;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+    game.run();
 }
