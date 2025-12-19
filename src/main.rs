af0269db0ca478dd658de380da3723686b24bbf6
Harrisonust <henhen.luo@gmail.com>
2025-12-19 22:46:21 +0800

    change [char;5] to String for simplicity, add valid words for input checks, refactor

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,91 @@
+const ROUND: u8 = 6;
+const WORD_LEN: usize = 5;
+
+#[derive(Clone)]
 enum State {
     Match,
     Exist,
     NotExist,
 }
+
+#[derive(Clone)]
 struct Word {
-    letters: [char; 5],
-    states: [State; 5],
+    letters: String, // make sure this has only 5 characters
+    states: [State; WORD_LEN],
 }
 
 struct Game {
     round: u8, // maximum 6 rounds
+    valid_words: Vec<String>,
     answer: String,
     history: Vec<Word>,
 }
 
 impl Game {
     fn new() -> Self {
+        let words = Game::load_words();
+        let answer = Game::draw_word(&words);
+
         Game {
-            round: 0,
-            answer: todo!(), // draw an answer
+            round: 1,
+            valid_words: words,
+            answer,
             history: Vec::new(),
         }
     }
 
-    fn compare(self, user: String) -> Word {
-        Word {
-            letters: todo!(),
-            states: todo!(),
-        }
+    fn load_words() -> Vec<String> {
+        todo!();
     }
 
-    fn update_screen(self) {}
+    fn draw_word(words: &[String]) -> String {
+        todo!();
+    }
 
-    fn parse_input(self, input: String) {}
+    fn compare(&self, user: &String) -> Word {
+        todo!();
+    }
+
+    fn update_screen(&self) {
+        todo!();
+    }
+
+    fn parse_input(&self, input: &str) -> Result<String, &str> {
+        todo!();
+    }
+
+    fn is_correct(&self, guess: &Word) -> bool {
+        todo!();
+    }
 
     fn run(&mut self) {
-        while self.round < 6 {
+        while self.round <= ROUND {
             // take user input
-            let user_input: String = todo!();
+            let user_input = String::new();
 
             // parsing
-            self.parse_input(user_input);
+            let guess = match self.parse_input(&user_input) {
+                Ok(g) => g,
+                Err(e) => {
+                    println!("Error: {e}");
+                    continue; // restart current round
+                }
+            };
 
             // compare
-            let result: Word = self.compare(user_input);
+            let result: Word = self.compare(&guess);
 
             // save result in history
-            self.history.push(result);
+            self.history.push(result.clone());
 
             // update screen
             self.update_screen();
 
             self.round += 1;
+
+            if self.is_correct(&result) {
+                break;
+            }
         }
     }
 }
